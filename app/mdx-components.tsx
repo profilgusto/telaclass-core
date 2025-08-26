824d57df7b3c0b22b14b8633cf78e494067b9188
Filipe Rocha <filipe@ufsj.edu.br>
2025-08-26 09:09:13 -0300

    impl: H1 headers now are centered

--- app/mdx-components.tsx
+++ app/mdx-components.tsx
@@ -35,7 +35,7 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
   };
   return {
     // Headings
-    h1: (p) => <h1 className="text-3xl font-bold mt-8 mb-4" {...p} />,
+  h1: (p) => <h1 className="text-3xl font-bold mt-8 mb-4 text-center" {...p} />,
     h2: (p) => <h2 className="text-2xl font-semibold mt-6 mb-3" {...p} />,
     h3: (p) => <h3 className="text-xl font-semibold mt-4 mb-2" {...p} />,
 
